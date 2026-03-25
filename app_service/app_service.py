a469604fcd624a431db5a9e007b4831353c7c769
eyllakdemir58-droid <eyll.akdemir58@gmail.com>
2026-03-25 13:43:36 +0300

    Update app_service.py

--- app_service/app_service.py
+++ app_service/app_service.py
@@ -1,1 +1,45 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import psycopg2, os
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+DATABASE_URL=os.getenv(
+   "DATABASE_URL",
+  "postgresql://eylul:lucz1erJ65bddtpS1EnDseH6mUhHWXg6@dpg-d6t8qmeuk2gs738m11t0-a.oregon-postgres.render.com/hello_cloud2_db_bh6p"
+)
+
+def connect_db():
+  return psycopg2.connect(DATABASE_URL)
+
+@app.route("/ziyaretciler",methods=["GET","POST"])
+def ziyaretciler():
+ conn = connect_db()
+ cur = conn.cursor()
+ cur.execute("CREATE TABLE IF NOT EXISTS ziyaretciler (id SERIAL PRIMARY KEY, isim
+TEXT)")
+
+
+ if request.method == "POST":
+    isim = request.json.get("isim")
+    if isim:
+         cur.execute("INSERT INTO ziyaretciler (isim) VALUES (%s)", (isim,))
+         conn.commit()
+
+         
+ cur.execute("SELECT isim FROM ziyaretciler ORDER BY id DESC LIMIT 10")
+ isimler = [row[0] for row in cur.fetchall()]
+
+ 
+ cur.close()
+ conn.close()
+
+
+  return jsonify(isimler)
+ 
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=5001)
 
